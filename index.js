3deecea0a5077cc20a9002f87274a117d2eda881
yashwanthyadav1901 <yashwanthyadav481@gmail.com>
2024-07-31 21:24:37 +0530

    setup finished

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const { error } = require("console");
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const app = express();
+const path = require("path");
+const connectDB = require("./config/dbConfig");
+const PORT = process.env.PORT;
+
+connectDB();
+
+app.use(express.json());
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status = 404;
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  }
+});
+
+mongoose.connection.once("open", () => {
+  console.log("database connected");
+  app.listen(PORT, () => {
+    console.log(`server listening on ${PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log(error);
+});
